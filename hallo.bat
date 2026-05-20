2e95c1e08bf8f41f0541d70ada93884ea5729714
drhochunendlich <benjberger@tsn.at>
2026-05-20 17:17:13 +0545

    Update hallo.bat

--- hallo.bat
+++ hallo.bat
@@ -1,4 +1,27 @@
 @echo off
+:: Pfad zum Autostart-Ordner definieren
+set "autostart_folder=%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+set "shortcut_name=HalloNachricht.lnk"
+
+:: Prüfen, ob die Verknüpfung im Autostart schon existiert
+if not exist "%autostart_folder%\%shortcut_name%" (
+    
+    :: Ein temporäres VBScript erstellen, das die Verknüpfung generiert
+    echo set WshShell = WScript.CreateObject("WScript.Shell") > "%temp%\MakeShortcut.vbs"
+    echo set oShellLink = WshShell.CreateShortcut("%autostart_folder%\%shortcut_name%") >> "%temp%\MakeShortcut.vbs"
+    echo oShellLink.TargetPath = "%~dpnx0" >> "%temp%\MakeShortcut.vbs"
+    echo oShellLink.WorkingDirectory = "%~dp0" >> "%temp%\MakeShortcut.vbs"
+    echo oShellLink.Save >> "%temp%\MakeShortcut.vbs"
+    
+    :: Das VBScript ausführen und danach löschen
+    cscript //nologo "%temp%\MakeShortcut.vbs"
+    del "%temp%\MakeShortcut.vbs"
+)
+
+:: ==========================================
+:: DER CODE FÜR DAS "HALLO!" FENSTER
+:: ==========================================
+
 :top
 explorer
 start explorer
